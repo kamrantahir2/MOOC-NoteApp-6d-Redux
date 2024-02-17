0cd72ccc5a60e29863841506056a5936af023b94
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-02-17 19:03:56 +0000

    Create function for updating note in requests.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { getNotes, createNote } from "./requests";
+import { getNotes, createNote, updateNote } from "./requests";
 
 const App = () => {
   const queryClient = useQueryClient();
