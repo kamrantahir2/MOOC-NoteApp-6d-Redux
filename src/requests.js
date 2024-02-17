63761c38827a97304cf4cc3fe0486ea058c357d0
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-02-17 19:35:14 +0000

    Use setQueryData to update state for frontend after new note is created or importance is change

--- src/requests.js
+++ src/requests.js
@@ -5,7 +5,7 @@ const baseUrl = "http://localhost:3001/notes";
 export const getNotes = () => axios.get(baseUrl).then((res) => res.data);
 
 export const createNote = (newNote) => {
-  axios.post(baseUrl, newNote).then((res) => res.data);
+  return axios.post(baseUrl, newNote).then((res) => res.data);
 };
 
 export const updateNote = (updatedNote) =>
